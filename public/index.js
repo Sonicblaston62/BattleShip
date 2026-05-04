beef0cf49ec82fe976d0185234d5c1800249d55a
Sonicblaston62 <bottols@outlook.com>
2026-05-04 23:27:03 +0200

    added grids for p1 and p2

--- public/index.js
+++ public/index.js
@@ -1,15 +1,31 @@
-// Grab the board div from HTML
-const board = document.getElementById('board');
+function createBoard(id) {
+  const board = document.getElementById(id);
+const board1 = document.getElementById('board1');
+const board2 = document.getElementById('board2');
+board.style.display = 'grid';
+board.style.gridTemplateColumns = 'repeat(10, 50px)';
 
-// Create a new div element
 
-// Give it a border
+for (let row = 0; row < 10; row++) {
+    for (let col = 0; col < 10; col++) {
 
-
-for (let i = 1; i <=10; i++) {
     const cell = document.createElement('div');
-    cell.style.border = '1px solid black';
+    cell.style.border = '2px solid black';
+    cell.style.margin = '5px'
     cell.style.width = '40px';
     cell.style.height = '40px';
+    cell.dataset.row = row;
+    cell.dataset.col = col;
+    cell.addEventListener('click', function() {
+        cell.style.backgroundColor = 'grey';
+
+    });
     board.appendChild(cell);
+
+    }
+
 }
+}
+
+createBoard('board1');
+createBoard('board2');// Grab the board div from HTML
